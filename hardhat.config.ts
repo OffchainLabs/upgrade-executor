b600bd0551370f68046a4c6516abe414b94a4085
gzeon <im@gzeon.dev>
2023-08-08 13:42:56 +0100

    chore: add hardhat and release typechain

---
+++ hardhat.config.ts
@@ -0,0 +1,32 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-foundry";
+
+// when changing optimizer settings, make sure to also change settings in foundry.toml
+const solidityProfiles = {
+  default: {
+    version: "0.8.16",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 20000
+      },
+    }
+  },
+}
+
+const config: HardhatUserConfig = {
+  solidity: solidityProfiles[process.env.FOUNDRY_PROFILE || "default"] || solidityProfiles.default,
+  paths: {
+    sources: "./src",
+    tests: "./test-ts",
+    cache: "./cache_hardhat",
+    artifacts: 'build/contracts',
+  },
+  typechain: {
+    outDir: 'build/types',
+    target: 'ethers-v5',
+  },
+};
+
+export default config;
