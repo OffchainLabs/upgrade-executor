99fba529dccbc873c3250781dd8836d41bf02dc1
gzeon <im@gzeon.dev>
2023-08-08 13:59:53 +0100

    chore: add IUpgradeExecutor

---
+++ src/IUpgradeExecutor.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity >=0.6.2 <0.9.0;
+
+interface IUpgradeExecutor {
+    function initialize(address admin, address[] memory executors) external;
+    function execute(address upgrade, bytes memory upgradeCallData) external payable;
+}
