457228038b3c16ec2595742543387b2b6e76ea37
Goran Vladika <goran.vladika@gmail.com>
2023-08-18 13:39:13 +0200

    Add executeCall to interface

--- src/IUpgradeExecutor.sol
+++ src/IUpgradeExecutor.sol
@@ -4,4 +4,5 @@ pragma solidity >=0.6.2 <0.9.0;
 interface IUpgradeExecutor {
     function initialize(address admin, address[] memory executors) external;
     function execute(address upgrade, bytes memory upgradeCallData) external payable;
+    function executeCall(address target, bytes memory targetCallData) external payable;
 }
